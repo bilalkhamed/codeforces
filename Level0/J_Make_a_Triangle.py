4ba10414cc46d60955e239c3665fb4cb89d61989
Bilal <bilalkhamed1@gmail.com>
2026-01-23 00:55:10 +0300

    Perf Sheet1 D:  remove unnecessary loop
    - use formula: large - (small1 + small2) + 1

--- Level0/J_Make_a_Triangle.py
+++ Level0/J_Make_a_Triangle.py
@@ -1,14 +1,7 @@
 a, b, c = map(int, input().split())
 
-if a+b>c and a+c>b and b+c>a:
+ls = sorted([a, b, c])
+if ls[0] + ls[1] > ls[2]:
   print(0)
 else:
-  ls = sorted([a, b, c])
-  flag = 0
-  while not (ls[0]+ls[1]>ls[2] and ls[0]+ls[2]>ls[1] and ls[1]+ls[2]>ls[0]):
-    if flag % 2 == 0:
-      ls[0] += 1
-    else:
-      ls[1] += 1
-    flag += 1
-  print(flag)
+  print(ls[2] - (ls[0] + ls[1]) + 1)
