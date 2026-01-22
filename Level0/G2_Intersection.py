36462495e65da661418b6f3ce6730f9b41ae19f1
Bilal <bilalkhamed1@gmail.com>
2026-01-23 00:15:27 +0300

    Fix Sheet1 G2: address all edge cases
    - use min and max to find start and end intervals
    - fix edge case where one interval is inside another

--- Level0/G2_Intersection.py
+++ Level0/G2_Intersection.py
@@ -1,21 +1,9 @@
 l1, r1, l2, r2 = map(int, input().split())
 
-if l2 > r1 or l1 > r2:
+start = max(l1, l2)
+end = min(r1, r2)
+
+if end < start:
   print('-1')
-elif (l1 > l2 and r1 > r2) or (l2 > l1 and r2 > r1):
-  print('here')
-  print(max(l1, l2), min(r1, r2))
 else:
-  if r1 > l2:
-    print(l2, r1)
-  else:
-    print(r1, l2)
-
-
-# Inside: (1, 10) U (3, 6) = 
-
-# (2, 8) U (6, 10)
-
-# (6, 10) U (2, 8)
-
-# No intersection: (2, 8) U (9, 10)
+  print(start, end)
